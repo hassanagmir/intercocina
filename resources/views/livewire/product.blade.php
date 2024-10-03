db06421b9792fd0acaa5a355136b2b71a61435be
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-03 10:18:22 +0100

    Chanage Product Layout gird

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -5,7 +5,7 @@
         <div class="swiper main-slide-carousel swiper-container relative mb-6">
             <div class="swiper-wrapper" id="gallery">
                 @foreach ($product->images as $image)
-                <a href="{{ Storage::url($image->image) }}" alt="{{ $product->name }}" class="swiper-slide pswp-gallery__item h-auto" data-pswp-width="1875" data-pswp-height="2500" target="_blank">
+                <a href="{{ Storage::url($image->image) }}" alt="{{ $product->name }}" class="swiper-slide pswp-gallery__item h-auto" data-pswp-width="1475" data-pswp-height="2000" target="_blank">
                     <img src="{{ Storage::url($image->image) }}" loading="lazy" title="{{ $product->name }}" alt="{{ $product->name }}" width="auto" height="auto" class="max-lg:mx-auto rounded-2xl m-auto max-h-[500px]">
                 </a>
                 @endforeach
