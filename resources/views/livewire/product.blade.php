c42fd66342442ddb1b1cdc7e9c7174834e31fc3b
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-04 14:54:09 +0100

    New updates

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -29,6 +29,16 @@
                     <h1 class="font-manrope font-bold sm:text-3xl text-2xl leading-10 text-gray-900 mb-2">{{ $product->name }}</h1>
                     <h2 class="font-normal text-base text-gray-500">{{ $product->type->name }}</h2>
                 </div>
+                @if (auth()->user()->hasRole("super_admin"))
+                <a  href="/admin/products/{{ $product->id }}/edit" class="group transition-all duration-500 p-0.5 sm:block hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                            <path class="stroke-green-600 transition-all duration-500 group-hover:stroke-green-700" d="M9.533 11.15A1.82 1.82 0 0 0 9 12.438V15h2.578c.483 0 .947-.192 1.289-.534l7.6-7.604a1.82 1.82 0 0 0 0-2.577l-.751-.751a1.82 1.82 0 0 0-2.578 0z"/>
+                            <path class="stroke-green-600 transition-all duration-500 group-hover:stroke-green-700" d="M21 12c0 4.243 0 6.364-1.318 7.682S16.242 21 12 21s-6.364 0-7.682-1.318S3 16.242 3 12s0-6.364 1.318-7.682S7.758 3 12 3"/></g>
+                        </svg>
+                </a>
+                    
+                @else
                 <button class="group transition-all duration-500 p-0.5 sm:block hidden">
                     <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle class="fill-red-50 transition-all duration-500 group-hover:fill-red-100" cx="30" cy="30" r="30" fill="" />
@@ -38,6 +48,8 @@
                             stroke-linejoin="round" />
                     </svg>
                 </button>
+                @endif
+
             </div>
             <p>{{ $product->description }}</p>
             <div class="flex flex-col min-[400px]:flex-row min-[400px]:items-center mb-5 gap-y-3 flex-wrap">
