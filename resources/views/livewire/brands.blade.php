6cddb5c478e60f1163e9546c27ef3ecf3f65c9b2
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-04 17:07:42 +0100

    New updates

--- resources/views/livewire/brands.blade.php
+++ resources/views/livewire/brands.blade.php
@@ -1,12 +1,9 @@
-<div>
-    <div class="md:max-w-5xl md:mx-auto">
-
-    
+<div class="md:max-w-5xl md:mx-auto">
     <h2 class="pt-20 pb-4 text-2xl font-bold text-center md:text-4xl" x-animate.intersect="fadeInUp">
         {{ __("Nos marques") }}
     </h2>
     <p class="text-xl text-center" x-animate.intersect="fadeInUp">Nos marques se distinguent par leur qualité, leur design innovant et un parfait équilibre entre style et fonctionnalité, offrant des solutions de cuisine élégantes et précises.</p>
-    </div>
+
 
 
     <ul class="grid gap-8 py-5 md:grid-cols-3">
