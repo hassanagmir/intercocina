aaeb50b2aac83cfcf3ba59056873b706396ac747
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-09 11:06:46 +0100

    Remove the unKnow links

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,10 +4,10 @@
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         @livewire('product', ['product' => $product], key($product->id))
         <div class="mt-6">
+
+            @if ($product->options || $product->content)
             <div class="p-4 rounded-xl border bg-white">
-                @if ($product->options || $product->content)
                 <h2 class="sm:text-2xl text-xl font-bold">{{ __("Détails du produit")}} {{ $product->name}}</h2>
-                @endif
                 @if ($product->options)
                 <div class="relative overflow-x-auto border-t-2 border-x-2 sm:rounded-lg mt-4">
                     <table class="w-full text-sm text-left rtl:text-right">
@@ -30,6 +30,7 @@
                     {!! $product->content !!}
                 </div>
             </div>
+            @endif
 
             {{-- Recomendation product --}}
             <h2 class="mb-2 mt-3 text-2xl font-black">
@@ -38,7 +39,6 @@
                 </span>
             </h2>
 
-            
             <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
                 @foreach ($products as $product)
                 <x-product-card name="{{ $product->name}}" price="{{ $product->price() }}"
@@ -46,12 +46,9 @@
                 @endforeach
             </div>
 
-
-
             <div class="bg-white border-2 p-4 rounded-2xl mt-10">
                 @livewire('rating-form', ['product' => $product], key($product->id))
             </div>
-
 
             @if(count($product->reviews))
                 {{-- Rating list --}}
