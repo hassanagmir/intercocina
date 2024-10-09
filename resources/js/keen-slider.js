8f61362dc9d0d3fa2abe379f8526bee789b5126a
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-09 10:49:10 +0100

    Remove the unKnow links

--- resources/js/keen-slider.js
+++
@@ -1,4 +0,0 @@
-import KeenSlider from 'keen-slider'
-import 'keen-slider/keen-slider.min.css'
-
-window.KeenSlider = KeenSlider;
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/tiptap.css',
                 'resources/js/app.js',
-                'resources/js/keen-slider.js',
                 'resources/css/filament/admin/theme.css',
             ],
             refresh: true,
