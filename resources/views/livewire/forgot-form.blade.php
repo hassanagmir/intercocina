9739fc1b68d0036275d255980ad38b25dabb2163
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-09 13:08:04 +0100

    New updaets

--- resources/views/livewire/forgot-form.blade.php
+++ resources/views/livewire/forgot-form.blade.php
@@ -3,7 +3,7 @@
         <h2 class="text-center text-3xl font-extrabold text-gray-900">
             {{ __("Mot de passe oublié ?") }}
         </h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
+        <p class="mt-2 text-center text-xl text-slate-700">
             {{ __("Ne vous inquiétez pas, nous vous enverrons des instructions de réinitialisation.") }}
         </p>
     </div>
--- resources/views/livewire/reset-form.blade.php
+++ resources/views/livewire/reset-form.blade.php
@@ -3,7 +3,7 @@
         <h2 class="text-center text-3xl font-extrabold text-gray-900">
             {{ __("Réinitialiser le mot de passe") }}
         </h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
+        <p class="mt-2 text-center text-xl text-slate-700">
             {{ __("Vous pouvez maintenant saisir votre nouveau mot de passe pour le réinitialiser") }}
         </p>
     </div>
