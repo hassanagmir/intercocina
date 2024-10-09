fbce2d74e6c39f43365bdf62a9e6d7360339acde
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-09 10:28:58 +0100

    Update Product details

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,29 +4,31 @@
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         @livewire('product', ['product' => $product], key($product->id))
         <div class="mt-6">
-            @if ($product->options || $product->content)
+            <div class="p-4 rounded-xl border bg-white">
+                @if ($product->options || $product->content)
                 <h2 class="sm:text-2xl text-xl font-bold">{{ __("Détails du produit")}} {{ $product->name}}</h2>
-            @endif
-            @if ($product->options)
-            <div class="relative overflow-x-auto border-t-2 border-x-2 sm:rounded-lg mt-4">
-                <table class="w-full text-sm text-left rtl:text-right">
-                    <tbody>
-                        @foreach ($product->options as $key => $value)
-                        <tr class="border-b-2 font-semibold hover:bg-gray-50 duration-500">
-                            <th scope="row" class="px-6 py-4 text-gray-900 whitespace-nowrap">
-                                {{ $key }}
-                            </th>
-                            <td class="px-6 py-4">
-                                {{ $value }}
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            @endif
-            <div class="mt-4 prose">
-                {!! $product->content !!}
+                @endif
+                @if ($product->options)
+                <div class="relative overflow-x-auto border-t-2 border-x-2 sm:rounded-lg mt-4">
+                    <table class="w-full text-sm text-left rtl:text-right">
+                        <tbody>
+                            @foreach ($product->options as $key => $value)
+                            <tr class="border-b-2 font-semibold hover:bg-gray-50 duration-500">
+                                <th scope="row" class="px-6 py-4 text-gray-900 whitespace-nowrap">
+                                    {{ $key }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $value }}
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                @endif
+                <div class="mt-4 prose">
+                    {!! $product->content !!}
+                </div>
             </div>
 
             {{-- Recomendation product --}}
