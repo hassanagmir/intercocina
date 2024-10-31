6c48d135f4317c62f76c40951f6667b2ac908e85
Hassan Agmir <hassaneagmir@gmail.com>
2024-10-31 12:33:20 +0100

    New updates

---
+++ app/Filament/Resources/ImageResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ImageResource\Pages;
+use App\Filament\Resources\ImageResource\RelationManagers;
+use App\Models\Image;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ImageResource extends Resource
+{
+    protected static ?string $model = Image::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getModelLabel(): string
+    {
+        return __("Images des produits");
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Forms\Components\Select::make('product_id')
+                    ->required()
+                    ->relationship('product', 'name')
+                    ->preload()
+                    ->searchable(),
+                Forms\Components\Select::make('color_id')
+                    ->relationship('color', 'name')
+                    ->preload()
+                    ->searchable(),
+
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('product.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('color.name')
+                    ->placeholder("__")
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListImages::route('/'),
+            'create' => Pages\CreateImage::route('/create'),
+            'edit' => Pages\EditImage::route('/{record}/edit'),
+        ];
+    }
+}
