c88930d7c96ecf8becf6191c1dbca32022901a9d
Hassan AGHMIR <hagmir6@gmail.com>
2024-12-25 12:23:52 +0100

    Update Same isuse

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -46,8 +46,8 @@
                                     <a href="/category/parquettes" title="Parquet" class="bg-neutral-50 px-4 py-2 text-sm text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-none" role="menuitem">
                                         Parquet
                                     </a>
-                                    <a href="/category/accessoiriser" title="Accessoiriser de cuisine" class="bg-neutral-50 px-4 py-2 text-sm text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-none" role="menuitem">
-                                        Accessoiriser
+                                    <a href="/category/accessoiriser" title="Accessoires de cuisine" class="bg-neutral-50 px-4 py-2 text-sm text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-none" role="menuitem">
+                                        Accessoires
                                     </a>
                                 </div>
                             </div>
