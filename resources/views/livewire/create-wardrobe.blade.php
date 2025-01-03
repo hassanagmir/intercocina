a7dcd2ad64b562f676dd4bdb93f45cd7d41e3f81
Hassan AGHMIR <hagmir6@gmail.com>
2025-01-03 12:08:08 +0100

    New update

--- resources/views/livewire/create-wardrobe.blade.php
+++ resources/views/livewire/create-wardrobe.blade.php
@@ -69,7 +69,7 @@
                 <div x-show="modalIsOpen" x-transition:enter="transition ease-out duration-200 delay-100 motion-reduce:transition-opacity" x-transition:enter-start="opacity-0 scale-50" x-transition:enter-end="opacity-100 scale-100" class="flex max-w-lg flex-col gap-4 overflow-hidden rounded-md border border-neutral-300 bg-white text-neutral-600 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
                     <!-- Dialog Header -->
                     <div class="flex items-center justify-between border-b border-neutral-300 bg-neutral-50/60 p-4 dark:border-neutral-700 dark:bg-neutral-950/20">
-                        <h3 id="defaultModalTitle" class="font-semibold tracking-wide text-neutral-900 dark:text-white">Special Offer</h3>
+                        <h3 id="defaultModalTitle" class="font-semibold tracking-wide text-neutral-900 dark:text-white"> Votre placard</h3>
                         <button @click="modalIsOpen = false" aria-label="close modal">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="1.4" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
@@ -78,41 +78,43 @@
                     </div>
                     <!-- Dialog Body -->
                     <div class="grid grid-cols-2 gap-4 p-4">
-                        <ul class="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 p-4">
+                        <ul class="max-w-md space-y-1 text-gray-500 dark:text-gray-400 p-4">
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Hauteur")}}</span>
-                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['height'] }}</span> 
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Hauteur")}}: </span>
+                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['height'] }} cm</span> 
                             </li>
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Largeur")}}</span>
-                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['width'] }}</span> 
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Largeur")}}: </span>
+                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['width'] }} cm</span> 
                             </li>
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Profondeur")}}</span>
-                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['depth'] }}</span> 
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Profondeur")}}: </span>
+                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['depth'] }} cm</span> 
                             </li>
 
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Epaisser")}}</span>
-                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['color'] ?  $result['color']->thickness : "N/E"  }}</span> 
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Epaisser")}} : </span>
+                                <span class="font-semibold text-gray-900 dark:text-white">{{ $result['color'] ?  $result['color']->thickness : "N/E"  }} mm</span> 
                             </li>
                         </ul>
 
-                        <ul class="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 p-4">
+                        <ul class="max-w-md space-y-1 text-gray-500 dark:text-gray-400 p-4">
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Couleur")}}</span>
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Couleur")}} : </span>
                                 <span class="font-semibold text-gray-900 dark:text-white">{{ $result['color'] ? $result['color']->name : "N/E" }}</span> 
                             </li>
                             <li>
-                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Ref de couleur")}}</span>
+                                <span class="font-semibold text-gray-600 dark:text-white">{{ __("Ref de couleur")}} : </span>
                                 <span class="font-semibold text-gray-900 dark:text-white">{{ $result['color'] ? $result['color']?->code : "N/E" }}</span> 
                             </li>
                         </ul>
                     </div>
                     <!-- Dialog Footer -->
                     <div class="flex flex-col-reverse justify-between gap-2 border-t border-neutral-300 bg-neutral-50/60 p-4 dark:border-neutral-700 dark:bg-neutral-950/20 sm:flex-row sm:items-center md:justify-end">
-                        <button @click="modalIsOpen = false" type="button" class="cursor-pointer whitespace-nowrap rounded-md px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-600 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:opacity-100 active:outline-offset-0 dark:text-neutral-300 dark:focus-visible:outline-white">Remind me later</button>
-                        <button @click="modalIsOpen = false" type="button" class="cursor-pointer whitespace-nowrap rounded-md bg-black px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:opacity-100 active:outline-offset-0 dark:bg-white dark:text-black dark:focus-visible:outline-white">Upgrade Now</button>
+                        <button @click="modalIsOpen = false" type="button" class="flex gap-3 items-center cursor-pointer whitespace-nowrap rounded-md bg-red-600 px-4 py-2 text-center text-sm font-medium tracking-wide text-neutral-100 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 active:opacity-100 active:outline-offset-0">
+                            <span>Valider</span>
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>
+                        </button>
                     </div>
                 </div>
             </div>
