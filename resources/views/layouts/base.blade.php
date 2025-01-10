f2bd64e6de07cc98b61dde3703965a27520a924f
Hassan AGHMIR <hagmir6@gmail.com>
2025-01-10 15:12:07 +0100

    Add google search console

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,23 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ (isset($title) ? $title . ' - ' : '') . config('app.name', 'Intercocina') }}</title>
-    <meta name="description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Fort d'une expérience de plus d'une décennie, Intercocina, leader en tant que fabricant de meubles de cuisine de lux." }}">
+    <meta name="description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Intercocina est une entreprise spécialisée dans la fabrication de meubles de cuisine, meubles TV, placards et armoires, meubles de salle de bain, ainsi que de parquets, au Maroc." }}">
     <meta name="keywords" content="{{ isset($tags) ? $tags : 'Mobile de cuisine, Caisson  , Facade, Placards, Parquets, Tiroirs, Armoire, Caissons Bas, Caissons Haut, Caissons column' }}">
     <meta itemprop="image" content="{{ isset($image) ? $image : asset('assets/imgs/intercocina-logo.png') }}"> 
     <link rel="icon" type="image/x-icon" href="{{ asset('assets\imgs\favicon.png') }}">
     <link rel="canonical" href="{{ request()->fullUrl() }}" />
+    <meta name="google-site-verification" content="qvGj4QxXzolE9UDquiCtVKxUzQqkKt92LtItABRoQBw" />
     {{-- Open Graph / Facebook --}}
     
     <meta property="og:url" content="{{ request()->fullUrl() }}">
     <meta property="og:title" content="{{ isset($title) ? $title : config('app.name', 'Intercocina') }}">
-    <meta property="og:description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Fort d'une expérience de plus d'une décennie, Intercocina, leader en tant que fabricant de meubles de cuisine de lux." }}">
+    <meta property="og:description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Intercocina est une entreprise spécialisée dans la fabrication de meubles de cuisine, meubles TV, placards et armoires, meubles de salle de bain, ainsi que de parquets, au Maroc." }}">
     <meta property="og:image" content="{{ isset($image) ? $image : asset('assets/imgs/intercocina-logo.png') }}">
     <meta property="og:type" content="{{ isset($type) ? $type: "website" }}">
     {{-- Twitter --}}
    
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:title" content="{{ isset($title) ? $title : config('app.name', 'Laravel') }}">
-    <meta property="twitter:description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Fort d'une expérience de plus d'une décennie, Intercocina, leader en tant que fabricant de meubles de cuisine de lux." }}">
+    <meta property="twitter:description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Intercocina est une entreprise spécialisée dans la fabrication de meubles de cuisine, meubles TV, placards et armoires, meubles de salle de bain, ainsi que de parquets, au Maroc." }}">
     <meta property="twitter:image" content="{{ isset($image) ? $image : asset('assets/imgs/intercocina-logo.png') }}">
     <meta property="twitter:url" content="{{ request()->fullUrl() }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
