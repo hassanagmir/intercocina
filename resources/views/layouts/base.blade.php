2b0d0329e6da4a169d26fa1dcca268b72b26e625
Hassan AGHMIR <hagmir6@gmail.com>
2025-01-13 10:21:00 +0100

    add Meta keywords

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="content-language" content="fr">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ (isset($title) ? $title . ' - ' : '') . config('app.name', 'Intercocina') }}</title>
     <meta name="description" content="{{ isset($description) ? Str::limit($description, 160, '') : "Intercocina est une entreprise spécialisée dans la fabrication de meubles de cuisine, meubles TV, placards et armoires, meubles de salle de bain, ainsi que de parquets, au Maroc." }}">
     <meta name="keywords" content="{{ isset($tags) ? $tags : 'Mobile de cuisine, Caisson  , Facade, Placards, Parquets, Tiroirs, Armoire, Caissons Bas, Caissons Haut, Caissons column' }}">
