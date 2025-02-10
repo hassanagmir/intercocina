af4fb2fc50696e8f070ca0de89dc7a255b234577
Hassan AGHMIR <hagmir6@gmail.com>
2025-02-10 11:15:37 +0100

    Add Products to blog article

--- resources/views/livewire/blog.blade.php
+++ resources/views/livewire/blog.blade.php
@@ -5,7 +5,7 @@
             <div class="col-span-full md:col-span-1">
                 <a class="block" href="{{ route('post.show', $post->slug )}}" target="_blank">
                     <div class="overflow-hidden w-full aspect-video bg-white rounded-lg border">
-                        <img loading="lazy" class="object-contain w-full aspect-video transform transition-transform duration-300 hover:scale-110" src="{{ url(config('app.storage'), $post->image) }}" alt="{{ $post->title }}">
+                        <img loading="lazy" class="w-full aspect-video transform transition-transform duration-300 hover:scale-110" src="{{ url(config('app.storage'), $post->image) }}" alt="{{ $post->title }}">
                     </div>
                     <h3 class="pt-4 text-xl font-bold uppercase">
                         {{ Str::limit($post->title, 50, '...') }}
