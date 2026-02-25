8f0c4a96eb7d8771ca92b42839deda5a0eef7aa4
Agmir Hassane <hagmir6@gmail.com>
2026-02-25 08:16:34 +0000

    app/Http/Resources/

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
 });
 
 Route::post('/register', [AuthController::class, 'register']);
